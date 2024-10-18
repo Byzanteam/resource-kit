928d7dfe9b917460139fafbc38782f6609dedfc7
vanppo <vanppo@hotmail.com>
2024-10-18 14:44:26 +0800

    fixup! chore: build release of resource_kit

--- README.md
+++ README.md
@@ -8,16 +8,16 @@
 
 | Name | Description | Default |
 | ---- | ----------- | ------- |
-| RESOURCE_KIT_DATABASE_URL | 数据库连接地址（格式：`ecto://USER:PASS@HOST/DATABASE`，[参考](https://hexdocs.pm/ecto/3.9.4/Ecto.Repo.html#module-urls)） | **required** |
-| RESOURCE_KIT_ECTO_IPV6 | Ecto 是否使用 IPv6 连接数据库 | `"false"` |
-| RESOURCE_KIT_SERVER_PORT | 接口服务监听的端口 | `4000` |
+| RESOURCE_KIT_CLI_DATABASE_URL | 数据库连接地址（格式：`ecto://USER:PASS@HOST/DATABASE`，[参考](https://hexdocs.pm/ecto/3.9.4/Ecto.Repo.html#module-urls)） | **required** |
+| RESOURCE_KIT_CLI_ECTO_IPV6 | Ecto 是否使用 IPv6 连接数据库 | `"false"` |
+| RESOURCE_KIT_CLI_SERVER_PORT | 接口服务监听的端口 | `4000` |
 
 ## Sentry
 
 | Name | Description | Default |
 | ---- | ----------- | ------- |
-| RESOURCE_KIT_SENTRY_DSN | Sentry 的 DSN（为了便于管理，每个 server 单独一个 DSN） | `N/A` |
-| RESOURCE_KIT_SENTRY_SERVER_NAME | Resource Kit 服务所在服务器的名字，便于排查出错的服务器 | `N/A` |
+| RESOURCE_KIT_CLI_SENTRY_DSN | Sentry 的 DSN（为了便于管理，每个 server 单独一个 DSN） | `N/A` |
+| RESOURCE_KIT_CLI_SENTRY_SERVER_NAME | Resource Kit 服务所在服务器的名字，便于排查出错的服务器 | `N/A` |
 
 ### Versions
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -8,20 +8,20 @@ See https://hexdocs.pm/ecto/Ecto.Repo.html#module-urls
 """
 
 if config_env() == :prod do
-  maybe_ipv6 = if cast_boolean("RESOURCE_KIT_ECTO_IPV6"), do: [:inet6], else: []
+  maybe_ipv6 = if cast_boolean("RESOURCE_KIT_CLI_ECTO_IPV6"), do: [:inet6], else: []
 
   config :resource_kit, ResourceKitCLI.Endpoint,
-    port: fetch_integer!("RESOURCE_KIT_SERVER_PORT", default: 4000)
+    port: fetch_integer!("RESOURCE_KIT_CLI_SERVER_PORT", default: 4000)
 
   config :resource_kit, ResourceKitCLI.Repo,
-    url: fetch_string!("RESOURCE_KIT_DATABASE_URL", hint: database_url_hint),
+    url: fetch_string!("RESOURCE_KIT_CLI_DATABASE_URL", hint: database_url_hint),
     socket_options: maybe_ipv6
 end
 
-if sentry_dsn = System.get_env("RESOURCE_KIT_SENTRY_DSN") do
+if sentry_dsn = System.get_env("RESOURCE_KIT_CLI_SENTRY_DSN") do
   config :sentry,
     dsn: sentry_dsn,
     release: System.get_env("APP_VERSION"),
     tags: %{revision: System.get_env("APP_REVISION")},
-    server_name: System.get_env("RESOURCE_KIT_SENTRY_SERVER_NAME")
+    server_name: System.get_env("RESOURCE_KIT_CLI_SENTRY_SERVER_NAME")
 end
