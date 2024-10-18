226ef64e069dbde6dd0c7e408de1e1c5a6d23040
vanppo <vanppo@hotmail.com>
2024-10-18 17:55:34 +0800

    [JET-2051] 让 resource_kit cli 工作起来 (#9)

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,9 @@ import Config
 
 config :resource_kit, ecto_repos: [ResourceKitCLI.Repo]
 
+config :sentry,
+  client: ResourceKitCLI.SentryClient,
+  enable_source_code_context: true,
+  root_source_code_paths: [File.cwd!()]
+
 import_config "#{config_env()}.exs"
