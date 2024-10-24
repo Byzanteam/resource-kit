78fffab0c45216bc4ef4d9a5069eb83e6482c823
vanppo <vanppo@hotmail.com>
2024-10-24 10:54:02 +0800

    refactor: extract route path from plug

--- lib/resource_kit_plug/router.ex
+++ lib/resource_kit_plug/router.ex
@@ -1,14 +1,9 @@
 defmodule ResourceKitPlug.Router do
   @moduledoc false
 
-  use Plug.Router
+  use Plug.Builder
 
   @dynamic_private :resource_kit_plug_dynamic_repo
-
-  plug Plug.Logger
-  plug Plug.Parsers, parsers: [{:json, json_decoder: Jason}]
-  plug :match
-  plug :dispatch
 
   @spec put_dynamic(conn :: Plug.Conn.t(), dynamic_repo :: ResourceKit.Repo.dynamic_repo()) ::
           Plug.Conn.t()
@@ -16,18 +11,17 @@ defmodule ResourceKitPlug.Router do
     put_private(conn, @dynamic_private, dynamic_repo)
   end
 
-  post "/rpc/actions", do: handle(conn, ResourceKitPlug.Service)
-
-  defp handle(%Plug.Conn{} = conn, service) do
+  @impl Plug
+  def call(%Plug.Conn{private: private} = conn, _opts) do
     import PhxJsonRpcWeb.Views.Helpers
 
-    case fetch_dynamic(conn) do
+    case Map.fetch(private, @dynamic_private) do
       {:ok, dynamic_repo} ->
         conn
         |> put_resp_header("content-type", "application/json; charset=utf-8")
         |> send_json(
           conn.params
-          |> service.handle(%{dynamic_repo: dynamic_repo})
+          |> ResourceKitPlug.Service.handle(%{dynamic_repo: dynamic_repo})
           |> render_json()
         )
 
@@ -40,7 +34,7 @@ defmodule ResourceKitPlug.Router do
           use Plug.Builder
 
           plug :put_dynamic
-          plug ResourceKitPlug.Router
+          post "/rpc", to: ResourceKitPlug.Router
 
           defp put_dynamic(conn, _opts) do
             ResourceKitPlug.Router.put_dynamic_repo(conn, MyApp.Repo)
@@ -49,10 +43,6 @@ defmodule ResourceKitPlug.Router do
         ```
         """
     end
-  end
-
-  defp fetch_dynamic(%Plug.Conn{private: private}) do
-    Map.fetch(private, @dynamic_private)
   end
 
   defp send_json(%Plug.Conn{} = conn, data) do
