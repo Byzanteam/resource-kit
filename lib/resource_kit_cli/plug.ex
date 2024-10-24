ab9ff17528d50a6a5be23c5035d14b8285811d13
vanppo <vanppo@hotmail.com>
2024-10-24 11:14:36 +0800

    [JET-2098] refactor: extract route path from plug (#11)

--- lib/resource_kit_cli/plug.ex
+++ lib/resource_kit_cli/plug.ex
@@ -1,12 +1,17 @@
 defmodule ResourceKitCLI.Plug do
   @moduledoc false
 
-  use Plug.Builder
+  use Plug.Router
   use Sentry.PlugCapture
 
+  plug Plug.Logger
+  plug Plug.Parsers, parsers: [{:json, json_decoder: Jason}]
+  plug :match
   plug :put_dynamic
-  plug ResourceKitPlug.Router
+  plug :dispatch
   plug Sentry.PlugContext
+
+  post "/rpc/actions", to: ResourceKitPlug.Router
 
   defp put_dynamic(conn, _opts) do
     ResourceKitPlug.Router.put_dynamic(conn, ResourceKit.Repo.adapter())
